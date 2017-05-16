2c796bfb3dc15457acba0fa39a5eab29ce8f1441
trent-swanson <contact@trentwilliamswanson.com>
2017-05-16 17:10:48 +1000

    initial unity file commit

---
+++ Final Defence Fortress/Assets/3rdPartyAssets/FollowCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public Transform target;
+	public float distance = 3.0f;
+	public float height = 3.0f;
+	public float damping = 5.0f;
+	public bool smoothRotation = true;
+	public bool followBehind = true;
+	public float rotationDamping = 10.0f;
+
+	void Start() {
+		Cursor.visible = false;
+	}
+
+	void LateUpdate () {
+		Vector3 wantedPosition;
+		if(followBehind)
+			wantedPosition = target.TransformPoint(0, height, -distance);
+		else
+			wantedPosition = target.TransformPoint(0, height, distance);
+
+		transform.position = wantedPosition;
+
+		if (smoothRotation) {
+			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+		}
+		else transform.LookAt (target, target.up);
+	}
+}
---
+++ Final Defence Fortress/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class PlayerController : MonoBehaviour {
+
+	public XboxController controller1;
+
+	public float moveSpeed = 10;
+	public float turnSpeed = 10;
+	public float jumpHeight = 5;
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	public float bulletSpeed = 6;
+
+	void Update () {
+		if (XCI.GetAxis(XboxAxis.LeftStickY, controller1) > 0) {
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+		if (XCI.GetAxis(XboxAxis.LeftStickY, controller1) < 0) {
+			transform.Translate (Vector3.back * moveSpeed * Time.deltaTime);
+		}
+		if (XCI.GetAxis(XboxAxis.LeftStickX, controller1) < 0) {
+			transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
+		}
+		if (XCI.GetAxis(XboxAxis.LeftStickX, controller1) > 0) {
+			transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+		}
+		if (XCI.GetAxis(XboxAxis.RightStickX, controller1) < 0) {
+			transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+		}
+		if (XCI.GetAxis(XboxAxis.RightStickX, controller1) > 0) {
+			transform.Rotate (Vector3.up * turnSpeed * Time.deltaTime);
+		}
+		if (XCI.GetButtonDown(XboxButton.A, controller1)) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpHeight, ForceMode.Impulse);
+		}
+		if (XCI.GetAxis(XboxAxis.RightTrigger, controller1) > 0) {
+			GameObject tempBullet = Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+			tempBullet.GetComponent<Rigidbody>().velocity = tempBullet.transform.forward * bulletSpeed;
+		}
+	}
+}
